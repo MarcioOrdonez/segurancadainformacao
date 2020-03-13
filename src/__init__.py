9935a7f9d25a2da14e34bdca6cec946e7756f1bb
marcio <marcio.v.ordonez@gmail.com>
2020-03-13 20:00:49 -0300

    Task 3 esperando criação do banco

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,23 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return "???"
 
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    app = Flask(__name__)
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    database_file = "sqlite:///banco de dados"
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+    db = SQLAlchemy(app)
+    with app.app_context():
+        from controllers.crud import module
+        app.register_blueprint(module, url_prefix="/")
+
+        db.create_all()
+    return app
+
+
+if __name__ == "__main__":
+    create_app().run(port=3000)
