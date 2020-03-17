ba4d6d7fe72e60a14e4cbd30509b08877bb63a45
marcio <marcio.v.ordonez@gmail.com>
2020-03-17 16:36:25 -0300

    banco e conexao

--- src/__init__.py
+++ src/__init__.py
@@ -4,18 +4,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     project_dir = os.path.dirname(os.path.abspath(__file__))
     database_file = "sqlite:////salao_cabelereiro"
     app.config["SQLALCHEMY_DATABASE_URI"] = database_file
-    db = SQLAlchemy(app)
     db.init_app(app)
     with app.app_context():
-        from controllers import module
+        from src.crud import module
         app.register_blueprint(module, url_prefix="/")
-    return app, db
+    return app
 
 
 if __name__ == "__main__":
--- src/crud.py
+++ src/crud.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import request
 
+from src import db
+
 module = Blueprint('crud', __name__)
 
 
@@ -11,6 +13,7 @@ def inserir():
     senha = request.form.get('senha')
     sexo = request.form.get('sexo')
     telefone = request.form.get('telefone')
+    db.execute
 
 @module.route("/get", methods=["GET"])
 def get():
