00926e9eab2b2dbbf3609e9a676d51fb221de005
Eduardo Nunes <edugenix@gmail.com>
2020-05-03 23:11:43 -0300

    modelos das tabelas das infos de usuarios

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     project_dir = os.path.dirname(os.path.abspath(__file__))
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://root:admin@localhost/salao_cabeleireiro"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://root:admin@localhost/lgpd"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
     db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
         from src.controllers.crud import crud_bp
@@ -19,5 +23,6 @@ def create_app():
         app.register_blueprint(crud_bp)
 
         manager = Manager(app)
+        manager.add_command('db', MigrateCommand)
 
         return app
