e2bef08f5c5dc4d1155ecf3b482438a34fbe656d
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2020-05-11 19:04:32 -0300

    script e estilo das páginas

---
+++ html cabelereiro/gerenciamentoPage.js
@@ -0,0 +1,16 @@
+
+var iframe=document.querySelector("#tela");
+function login(){
+    iframe.setAttribute("src","/login.html");
+}
+
+function agenda(){
+    iframe.setAttribute("src","/agendamento.html");
+}
+
+function servico(){
+    iframe.setAttribute("src","/servico.html");
+}
+function perfil(){
+    iframe.setAttribute("src","/usuario.html");
+}
