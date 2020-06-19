b25a9b2af8cd4779162bf6f56de012b65f186113
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-06-19 19:00:47 -0300

    criacao e integracao das paginas de login e registro

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -12,62 +12,67 @@ auth_module = Blueprint('auth', __name__, url_prefix="/auth",
                     template_folder='../templates')
 
 
-@auth_module.route("/login", methods=["GET"])
+@auth_module.route("/login", methods=["GET","POST"])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        cripto= Criptografia()
+        email = request.form.get('email')
+        password = request.form.get('senha')
+
+        user = Usuario.query.filter_by(email=email).first()
+
+        if not user:
+            flash('Por favor cheque suas informações e tente novamente! ')
+            return redirect(url_for('auth.login'))
+        else:
+            password = cripto.criptografar(user.chave_publica, password)
+            if password == user.password:
+                return 'logado'
+
+        return 'tela principal depois de logado'
 
 
-@auth_module.route('/login', methods=['POST'])
-def login_post():
-    cripto= Criptografia()
-    email = request.form.get('email')
-    password = request.form.get('password')
+@auth_module.route('/registrar', methods=["GET","POST"])
+def registrar():
+    if request.method == 'GET':
+        return render_template('registrar.html')
+    if request.method == 'POST':
+        cripto= Criptografia()
+        chaves=cripto.criaChaves()
+        chave_publica = chaves["public"]
+        chave_privada = chaves["private"]
 
-    user = Usuario.query.filter_by(email=email).first()
+        user_email = request.form.get('email')
+        name = cripto.criptografar(chave_publica,request.form.get('nome'))
+        password = request.form.get('senha')
+        data_nascimento = datetime.datetime.strptime(request.form.get('data'),'%Y-%d-%m').date()
+        data_nascimento = cripto.criptografar(chave_publica,data_nascimento)
+        # data_nascimento = cripto.criptografar(chave_publica,datetime.datetime.now())
+        cpf = cripto.criptografar(chave_publica,request.form.get('cpf'))
 
-    if not user:
-        flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))
-    else:
-        password = cripto.criptografar(user.chave_publica, password)
-        if password == user.password:
-            return 'logado'
+        user = Usuario.query.filter_by(email=user_email).first()
 
-    return 'tela principal depois de logado'
+        if user:
+            flash('Email de usuario ja existe!')
+            return redirect(url_for('auth.signup'))
+
+        new_user = Usuario(nome=name, password=cripto.criptografar(chave_publica,password),
+                            email=user_email, funcionario=True,
+                            data_nascimento=data_nascimento, cpf=cpf, chave_publica=chave_publica )
+
+        db.session.add(new_user)
+        usuario = Usuario.query.filter_by(email=user_email).first()
+        tabela_chaves = Tabela_chaves(id_usuario = usuario.id_usuario, chave_privada = chave_privada)
+        db.session.add(tabela_chaves)
+        db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 
-@auth_module.route('/signup')
-def signup():
-    return render_template('usuario.html')
-
-@auth_module.route('/signup', methods=['POST'])
-def signup_post():
-    cripto= Criptografia()
-    chaves=cripto.criaChaves()
-    chave_publica = chaves["public"]
-    chave_privada = chaves["private"]
-
-    user_email = request.form.get('email')
-    name = cripto.criptografar(chave_publica,request.form.get('name'))
-    password = request.form.get('password')
-    # data_nascimento = request.form.get('data_nascimento')
-    data_nascimento = cripto.criptografar(chave_publica,datetime.datetime.now())
-    cpf = cripto.criptografar(chave_publica,request.form.get('cpf'))
-
-    user = Usuario.query.filter_by(email=user_email).first()
-
-    if user:
-        flash('Email address already exists.')
-        return redirect(url_for('auth.signup'))
-
-    new_user = Usuario(nome=name, password=cripto.criptografar(chave_publica,password),
-                        email=user_email, funcionario=True,
-                        data_nascimento=data_nascimento, cpf=cpf, chave_publica=chave_publica )
-
-    db.session.add(new_user)
-    usuario = Usuario.query.filter_by(email=user_email).first()
-    tabela_chaves = Tabela_chaves(id_usuario = usuario.id_usuario, chave_privada = chave_privada)
-    db.session.add(tabela_chaves)
-    db.session.commit()
-
-    return "vai pra tela de login"
+@auth_module.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'tela inicial'
